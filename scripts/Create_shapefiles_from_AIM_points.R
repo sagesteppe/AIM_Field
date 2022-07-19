682a006a35de8c44adba95b7299e2445ee0ca725
reedbenkendorf27@gmail.com <steppe>
2022-07-19 10:58:03 -0600

    created zip shape files for NRCS - their end certainly has a bug

--- scripts/Create_shapefiles_from_AIM_points.R
+++ scripts/Create_shapefiles_from_AIM_points.R
@@ -1,6 +1,10 @@
-library(tidyverse)
-library(sf)
-library(here)
+shhh <- suppressPackageStartupMessages
+
+shhh(library(tidyverse))
+shhh(library(sf))
+shhh(library(here))
+
+rm(shhh)
 
 initial_process <- readxl::read_excel(paste0(here(),  '/data/raw/PlotTracking_AIM_plots_2022-COPY.xlsx'), 
                               sheet = 3) %>% 
@@ -9,6 +13,7 @@ initial_process <- readxl::read_excel(paste0(here(),  '/data/raw/PlotTracking_AI
 initial_process <- initial_process %>% 
   drop_na(long:lat) %>% 
   filter(long != 'SAMPLED IN 2018') %>% 
+  mutate(across(c(long:lat),  as.numeric)) %>% 
   st_as_sf(coords = c(x = 'long', y = 'lat'), crs = 4269, remove = F) %>% 
   select(any_of(c('plot_id', 'panel', 'long', 'lat', 'next_in_line_or_oversample'))) %>% 
   rename('Point_Type' = next_in_line_or_oversample)
@@ -28,18 +33,43 @@ aim_pts_ssurgo <- initial_process %>%
               dplyr::select(plot_id, Point_Type) %>% 
               st_drop_geometry(), .) %>% 
   rename('geometry' = x, 'PartName' = plot_id) %>% 
-  st_as_sf()
-  
+  st_as_sf() %>% 
+  st_transform(4269)
+
 ggplot(aim_pts_ssurgo) +
   geom_sf(aes(fill = Point_Type)) +
   theme_bw()
 
-st_write(aim_pts_ssurgo %>% 
-           filter(Point_Type == 'Yes'), paste0(here(), '/data/raw/AOI_AIM_22_base.shp'),
-         quiet = T, append = F)
+# library(leaflet)
+#leaflet::leaflet(data = initial_process) %>% 
+#  addTiles() %>%
+#  addMarkers(~long, ~lat, 
+#             popup = ~as.character(plot_id), 
+#             label = ~as.character(plot_id))
 
+fname <- 'AOI_AIM_22_base'
 st_write(aim_pts_ssurgo %>% 
-           filter(Point_Type == 'Yes'), paste0(here(), '/data/raw/AOI_AIM_22_over.shp'),
+           filter(Point_Type == 'Yes'), paste0(here(), '/data/raw/', fname, '.shp'),
          quiet = T, append = F)
+zip(paste0(here(), '/data/raw/', fname), 
+    paste0(path = paste0(here(), '/data/raw/'),
+          list.files(path = paste0(here(), '/data/raw/'), pattern = fname)))
+list.files(path = paste0(here(), '/data/raw/'), pattern = fname)
+fs <- list.files(path = paste0(here(), '/data/raw/'), pattern = fname)
+fs <- fs[grep("zip", fs, invert = T)]
+file.remove(paste0(here(), '/data/raw/', fs))
 
-rm(initial_process)
+
+fname <- 'AOI_AIM_22_over'
+st_write(aim_pts_ssurgo %>% 
+           filter(Point_Type == 'Oversample'), paste0(here(), '/data/raw/', fname, '.shp'),
+         quiet = T, append = F)
+zip(paste0(here(), '/data/raw/', fname), 
+    paste0(path = paste0(here(), '/data/raw/'),
+          list.files(path = paste0(here(), '/data/raw/'), pattern = fname)))
+list.files(path = paste0(here(), '/data/raw/'), pattern = fname)
+fs <- list.files(path = paste0(here(), '/data/raw/'), pattern = fname)
+fs <- fs[grep("zip", fs, invert = T)]
+file.remove(paste0(here(), '/data/raw/', fs))
+
+rm(fname, initial_process, aim_pts_ssurgo, fs)
